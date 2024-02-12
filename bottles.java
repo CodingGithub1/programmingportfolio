b6e25ea3dd886bf2b8700700acf7a1ae6fd9591a
CodingGithub1 <142618730+CodingGithub1@users.noreply.github.com>
2024-02-12 09:35:00 -0700

    Add files via upload

---
+++ bottles.java
@@ -0,0 +1,8 @@
+class Main {
+
+  public static void main(String[] args) {
+        for (int bottles = 99; bottles > 0; bottles--){
+          System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of root beer. Take one down and pass it around.");
+        }
+  }
+}
