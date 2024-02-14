e353cb22437d1549608c9f07de76cca59bb4da33
CodingGithub1 <142618730+CodingGithub1@users.noreply.github.com>
2024-02-14 09:31:25 -0700

    Delete bottles.java

--- bottles.java
+++
@@ -1,8 +0,0 @@
-class Main {
-
-  public static void main(String[] args) {
-        for (int bottles = 99; bottles > 0; bottles--){
-          System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of root beer. Take one down and pass it around.");
-        }
-  }
-}
